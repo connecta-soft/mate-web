f6931f0ef790cd7eb3b82235192d09ee1cade5cd
Bakhtiyor <baxtikdev@gmail.com>
2024-11-15 21:50:45 +0500

    update

--- main/serializers.py
+++ main/serializers.py
@@ -286,24 +286,6 @@ class Leads2CreateSerialzier(serializers.ModelSerializer):
             text_content = f""""
             First Name: First
             Last Name: Last
-            Email: { lead.email }
-            Phone: { lead.nbm }
-            
-            Moving From City: { lead.ship_from.name.en }
-            Moving From State: { lead.ship_from.state.code }
-            Moving From Zip Code: { lead.ship_from.zip }
-            
-            Moving To City: { lead.ship_to.name.en }
-            Moving To State: { lead.ship_to.state.code }
-            Moving To Zip Code: { lead.ship_to.zip }
-            
-            Move Date: { lead.date.strftime("%m/%d/%Y") }
-            Make: { lead.vehicle }
-            Model: { lead.vehicle_model }
-            Year: { lead.car_year }
-            
-            Type Of Carrier: { t }
-            Running Condition: { lead.vehicle_runs }
             Type: Unselect
             """
             # html_content = html_templ.render(context=validated_data)
